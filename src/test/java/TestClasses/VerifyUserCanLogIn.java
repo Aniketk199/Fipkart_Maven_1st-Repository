ad31f99fefbee1a4d5cc081a5a63acdf53552958
Aniket Kor <aniketkor10@gmail.com>
2022-07-28 19:05:18 +0530

    add spaces

--- src/test/java/TestClasses/VerifyUserCanLogIn.java
+++ src/test/java/TestClasses/VerifyUserCanLogIn.java
@@ -25,9 +25,11 @@ public class VerifyUserCanLogIn {
 	
 	static WebDriver driver;
 	LoginPage lp;
-	
+
 	ExtentHtmlReporter ExtentReporter;
+	
 	ExtentReports report;
+	
 	ExtentTest test;
 
 	@BeforeClass
